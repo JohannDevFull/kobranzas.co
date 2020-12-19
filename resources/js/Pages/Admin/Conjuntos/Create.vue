6fc7f2fcc8a426337ecee7fa4d4c965926c0f75d
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-19 12:37:00 -0500

    Cambio diseño formulario crear conjunto

--- resources/js/Pages/Admin/Conjuntos/Create.vue
+++ resources/js/Pages/Admin/Conjuntos/Create.vue
@@ -1,75 +1,90 @@
-<template>
+ <template>
   <app-layout>
     <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin: auto;width: 400px;">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin: auto;width: 800px;">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Editar Conjunto</h3>
+              <h3 class="card-title">Crear nuevo conjunto</h3>
             </div>
 
-            <form role="form" method="POST" @submit.prevent="store">
-              <div class="card-body">
-                <div class="form-group">
-                  <label>Nombre del Conjunto:</label>
-                  <span class="required">*</span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Ingrese el nombre"
-                    v-model="name"
-                  />
+            <div class="card-body" style="padding: 0px;"> 
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                <div class="widget-user-header text-white"
+                     style="background: url('/storage/img/photo1.png') center center;height: 200px;">
                 </div>
-                <div class="form-group">
-                  <label>Direccion del conjunto:</label>
-                  <span class="required">*</span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Ingrese la direccion del conjunto"
-                    v-model="address"
-                  />
-                </div>
-               
-                <div class="form-group">
-                  <label>Telefono:</label>
-                  <span class="required">*</span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Ingrese un numero de telefono"
-                    v-model="phone"
-                  />
-                </div>
+                
+                <form role="form" method="POST" @submit.prevent="store">
+                  <div class="card-body row">
+                    
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Nombre del Conjunto:</label>
+                        <span class="required">*</span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Ingrese el nombre"
+                          v-model="name"
+                        />
+                      </div>
+                      
+                      <div class="form-group">
+                        <label>Direccion del conjunto:</label>
+                        <span class="required">*</span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Ingrese la direccion del conjunto"
+                          v-model="address"
+                        />
+                      </div>
+                    </div>
+                     
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Telefono:</label>
+                        <span class="required">*</span>
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="Ingrese un numero de telefono"
+                          v-model="phone"
+                        />
+                      </div>
 
-                <div class="form-group">
-                  <label>Administrador</label>
-                  <span class="required">*</span>
-                  <select class="form-control" v-model="selected">
-                    <option value="" disabled>Seleccione el Administrador</option>
-                    <option v-for="option in administradores" v-bind:value="option.id">
-                      {{ option.name }}
-                    </option>
-                  </select>
-                </div>
-                 
-              </div>
-              <!-- /.card-body -->
+                      <div class="form-group">
+                        <label>Administrador</label>
+                        <span class="required">*</span>
+                        <select class="form-control" v-model="selected">
+                          <option value="" disabled>Seleccione el Administrador</option>
+                          <option v-for="option in administradores" v-bind:value="option.id">
+                            {{ option.name }}
+                          </option>
+                        </select>
+                      </div>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Crear conjunto</button>
+                     
+                  </div>
+                  <!-- /.card-body -->
 
-              <div class="card-footer">
-                <button type="submit" class="btn btn-primary">Registrar</button>
-              </div>
-              
-              
-                <ul v-for="error in errors.errors">
-                  <li class="required">{{ error[0] }}</li>
-                </ul>
-              
-              <!-- <span  class="text-danger">{{
-                error[0]
-              }}</span> -->
-            </form>
+                  <div class="card-footer">
+                    
+                  </div>
+                      
+                    <ul v-for="error in errors.errors">
+                      <li class="required">{{ error[0] }}</li>
+                    </ul>
+                  
+                  <!-- <span  class="text-danger">{{
+                    error[0]
+                  }}</span> -->
+                </form>
+            </div>
 
+            
           </div>
         </div>
       </div>
@@ -81,6 +96,7 @@ import AppLayout from "@/Layouts/AppLayout";
 import JetNavLink from "@/Jetstream/NavLink";
 
 export default {
+  props: ['conjunto'],
   components: {
     AppLayout,
     JetNavLink,
@@ -89,13 +105,13 @@ export default {
     this.buscarResultados()
   },
   data(){
-    return { 
+    return {  
+      administradores: [],
       selected:"",
       name: "",
       address: "",
       phone: "",
       administrador: "",
-      administradores: [],
       errors: [],
     };
   },
@@ -103,22 +119,21 @@ export default {
     buscarResultados(){
       axios.get('/buscar/administradores',{
             params:{
-              idconjunto:this.buscar
+              buscar:this.buscar
             }
       })
       .then( res => { 
-          this.administradores=res.data 
-          alert("holaaas mi mundo");
+          this.administradores=res.data  
       })
       .catch( error => {
           console.log( error.response )
       });
-    },
+    }, 
     reset(){
       this.name = "";
       this.address = "";  
       this.phone = ""; 
-      this.administrador = ""; 
+      this.selected="";
     },
     store() {
       var url = "/conjuntos/store";
@@ -140,6 +155,8 @@ export default {
             subtitle: 'ok',
             body: 'Exito al crear nuevo conjunto.'
           }); 
+
+          this.reset();
           
         })
         .catch((error) => {
