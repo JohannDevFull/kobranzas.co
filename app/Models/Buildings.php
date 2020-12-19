c920cece8f98fb659377d7df0ba6e14a4edfcc3e
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-19 11:43:22 -0500

    Eliminar migracion conjuntos

--- app/Models/Buildings.php
+++ app/Models/Buildings.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Buildings extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_building';
+    
     protected $fillable = [
-        'name',
-        'address' , 
+        'name_building',
+        'address_building' , 
         'phone_building',
         'administrator_id',
     ];
+
 }
--- database/migrations/2020_12_19_162340_create_buildings_table.php
+++ database/migrations/2020_12_19_162340_create_buildings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateConjuntosTable extends Migration
+class CreateBuildingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateConjuntosTable extends Migration
     {
         Schema::create('buildings', function (Blueprint $table) {
             $table->increments('id_building');
-            $table->string('name');
+            $table->string('name_building');
             $table->string('address_building');
             $table->string('phone_building');
             $table->foreignId('administrator_id')->references('id')->on('users'); 
@@ -26,10 +26,10 @@ class CreateConjuntosTable extends Migration
     /**
      * Reverse the migrations.
      *
-     * @return void 
+     * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('conjuntos');
+        Schema::dropIfExists('buildings');
     }
 }
