17e5559900567f717da102f47f33b42e5f63b570
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-21 09:09:28 -0500

    Seerder state

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	$estados=['ACUERDO PAGO','ACUERDO ESCRITO','ACUERDO ROTO','COMPROMISO DE LLAMADA','COMPORMISO DE PAGO','CUENTA DE COBRO','DEMANDADO','DEMANDAR','FORMALIZAR','ILOCALIZADO','NOTIFICACION 2','PAZ Y SALVO','MINIMA CUANTIA','ORDENA SACAR','REDIFERIR','FORMALIZAR PAZ Y SALVO','SIN ESPECIFICAR'];
+    	$tamano=sizeof($estados);
+        for ($i=0; $i < $tamano ; $i++) 
+        {  
+             
+             //
+	        DB::table('state')->insert([ 
+	            'description' => $estados[$i],
+	        ]);  
+        }
+    }
+}
