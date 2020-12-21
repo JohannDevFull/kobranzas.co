3f0446808ce77ada55d064322bfc9953ecdba437
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-21 09:11:56 -0500

    Migracion clients

---
+++ database/migrations/2020_12_20_000002_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id_client');
+            $table->string('client_code')->unique();
+            $table->string('contract_number');
+            $table->foreignId('state_id')->references('id_state')->on('state');
+            $table->foreignId('user_id')
+            ->references('id')->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreignId('building_id')->references('id_building')->on('buildings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
