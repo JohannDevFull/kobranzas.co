5190049787f584c831ef31cd9fb93fcf41e80ecb
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-23 09:49:41 -0500

    Metodo para cargar administradores

--- app/Http/Controllers/BuildingsController.php
+++ app/Http/Controllers/BuildingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Buildings;
 use App\Models\Clients;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -31,6 +32,17 @@ class BuildingsController extends Controller
                             ->join('users','buildings.administrator_id', '=', 'id')
                             ->get();
         return response()->json($conjuntos);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function cargarAdministradores()
+    {    
+        $administradores=User::role('AdminConjunto')->get(); 
+        return response()->json($administradores);
     }
 
     /**
