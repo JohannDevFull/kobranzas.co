f4b32b2f7b809e28f2425cb3295c0a1aa5003b49
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-23 08:54:39 -0500

    Tabla para acuerdos

---
+++ database/migrations/2020_12_22_060041_create_agreements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgreementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agreements', function (Blueprint $table) {
+            $table->bigIncrements('id_agreement');  
+            $table->foreignId('account_id')->references('id_account')->on('accounts'); 
+            $table->bigInteger('employee_id'); 
+            $table->integer('number_dues');  
+            $table->integer('intereses');   
+            $table->integer('honorarios');  
+            $table->string('observations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agreements');
+    }
+}
