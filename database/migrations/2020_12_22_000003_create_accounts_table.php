f109801106f43fee310f463a3dc0b76e2f92b562
Johann Ramìrez <johann.devfull@gmail.com>
2020-12-23 08:37:53 -0500

    Tabla para cuentas

---
+++ database/migrations/2020_12_22_000003_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id_account');  
+            $table->foreignId('client_id')->references('id_client')->on('clients');
+            $table->decimal('capital_debt', $precision = 13, $scale = 4);  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
