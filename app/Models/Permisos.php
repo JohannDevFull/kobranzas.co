4e20ebbee4392261598257e1090e50147c2956d8
Johann Ramìrez <johann.devfull@gmail.com>
2021-01-26 00:09:08 -0500

    Methodo generar arreglo extracto

--- app/Models/Permisos.php
+++ app/Models/Permisos.php
@@ -55,6 +55,114 @@ class Permisos extends Model
         return $arregloRP;
     }
 
+    public static function extracto($id) 
+    {
+        $arregloRP;
+        $total_deuda;
+
+        $movimientos=DB::select('SELECT id_movement,created_at,type_movement_id,description_movement,valor_movement FROM movements  WHERE user_id='.$id);
+        $tipo_movimiento=DB::select('SELECT * FROM type_movement ');
+
+        $debito=$tipo_movimiento[0]->name_movement;
+        $credito=$tipo_movimiento[1]->name_movement;
+         
+        
+        $tamano=sizeof($movimientos);
+
+        for ($k=0; $k < $tamano ; $k++) 
+        {  
+
+                if($movimientos[$k]->type_movement_id==1)
+                {  
+                    if ($k==0) {
+                        $total_deuda=$movimientos[$k]->valor_movement;
+                    }else{
+                        $total_deuda=$total_deuda+$movimientos[$k]->valor_movement;
+                    }
+
+                     $arregloRP[$k]['id_movement'] = $movimientos[$k]->id_movement;
+                     $arregloRP[$k]['created_at'] =$movimientos[$k]->created_at;
+                     $arregloRP[$k]['type_movement_id'] = $debito;
+                     $arregloRP[$k]['description_movement'] = $movimientos[$k]->description_movement;
+                     $arregloRP[$k]['valor_cargue'] = $movimientos[$k]->valor_movement;
+                     $arregloRP[$k]['valor_abono'] = ""; 
+                     $arregloRP[$k]['total_deuda'] = $total_deuda; 
+                     
+                }
+                else 
+                {    
+                     
+                    $total_deuda=$total_deuda-$movimientos[$k]->valor_movement;
+                    
+
+                     $arregloRP[$k]['id_movement'] = $movimientos[$k]->id_movement;
+                     $arregloRP[$k]['created_at'] =$movimientos[$k]->created_at;
+                     $arregloRP[$k]['type_movement_id'] = $credito;
+                     $arregloRP[$k]['description_movement'] = $movimientos[$k]->description_movement;
+                     $arregloRP[$k]['valor_cargue'] = "";
+                     $arregloRP[$k]['valor_abono'] = $movimientos[$k]->valor_movement; 
+                     $arregloRP[$k]['total_deuda'] = $total_deuda; 
+                }
+            
+        }
+
+        return $arregloRP;
+    }
+
+    public static function informe($id) 
+    {
+        $arregloRP;
+        $total_deuda;
+
+        $movimientos=DB::select('SELECT id_movement,created_at,type_movement_id,description_movement,valor_movement FROM movements  WHERE user_id='.$id);
+        $tipo_movimiento=DB::select('SELECT * FROM type_movement ');
+
+        $debito=$tipo_movimiento[0]->name_movement;
+        $credito=$tipo_movimiento[1]->name_movement;
+         
+        
+        $tamano=sizeof($movimientos);
+
+        for ($k=0; $k < $tamano ; $k++) 
+        {  
+
+                if($movimientos[$k]->type_movement_id==1)
+                {  
+                    if ($k==0) {
+                        $total_deuda=$movimientos[$k]->valor_movement;
+                    }else{
+                        $total_deuda=$total_deuda+$movimientos[$k]->valor_movement;
+                    }
+
+                     $arregloRP[$k]['id_movement'] = $movimientos[$k]->id_movement;
+                     $arregloRP[$k]['created_at'] =$movimientos[$k]->created_at;
+                     $arregloRP[$k]['type_movement_id'] = $debito;
+                     $arregloRP[$k]['description_movement'] = $movimientos[$k]->description_movement;
+                     $arregloRP[$k]['valor_cargue'] = $movimientos[$k]->valor_movement;
+                     $arregloRP[$k]['valor_abono'] = ""; 
+                     $arregloRP[$k]['total_deuda'] = $total_deuda; 
+                     
+                }
+                else 
+                {    
+                     
+                    $total_deuda=$total_deuda-$movimientos[$k]->valor_movement;
+                    
+
+                     $arregloRP[$k]['id_movement'] = $movimientos[$k]->id_movement;
+                     $arregloRP[$k]['created_at'] =$movimientos[$k]->created_at;
+                     $arregloRP[$k]['type_movement_id'] = $credito;
+                     $arregloRP[$k]['description_movement'] = $movimientos[$k]->description_movement;
+                     $arregloRP[$k]['valor_cargue'] = "";
+                     $arregloRP[$k]['valor_abono'] = $movimientos[$k]->valor_movement; 
+                     $arregloRP[$k]['total_deuda'] = $total_deuda; 
+                }
+            
+        }
+
+        return $arregloRP;
+    }
+
     public static function roles($value='')
     {
        $roles=DB::select('SELECT id,name FROM roles');
