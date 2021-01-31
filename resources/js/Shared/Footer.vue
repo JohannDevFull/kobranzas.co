5b410c4adc3b70efb2c2bd3bfde84335f09ad077
Johann Ramìrez <johann.devfull@gmail.com>
2021-01-31 16:58:21 -0500

    Atribucion copy rigth jtwo.tk

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -2,7 +2,7 @@
  <div>
     <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020-2020 <a href="http://adminlte.io">kobranzas.com</a>.</strong>
+    <strong>Copyright &copy; 2020-2021 <a href="http://jtwo.tk">jtwo.tk</a>.</strong>
     All rights reserved.
     
     <div class="float-right d-none d-sm-inline-block">
